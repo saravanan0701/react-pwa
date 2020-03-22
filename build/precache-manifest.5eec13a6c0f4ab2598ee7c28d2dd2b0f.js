8278c6d340bc8dfcb8cb67d4e7d7eb8472708528
saravanan <ls.saravanan96@gmail.com>
2020-03-22 16:37:01 +0530

    boiler plate react PWA

---
+++ build/precache-manifest.5eec13a6c0f4ab2598ee7c28d2dd2b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e856a65873f9a22f7bec03bf3b7642c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f5e6da0548bfb0ae39",
+    "url": "/static/css/main.9a8677a3.chunk.css"
+  },
+  {
+    "revision": "6f5669ed13c8a406ef5e",
+    "url": "/static/js/2.6d17362c.chunk.js"
+  },
+  {
+    "revision": "c8f5e6da0548bfb0ae39",
+    "url": "/static/js/main.71c36116.chunk.js"
+  },
+  {
+    "revision": "3963c64d7d9a72329f14",
+    "url": "/static/js/runtime~main.da0f25bc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [ toggle , handleToggle ] = useState(false);
+  const [ toggle1 , handleToggle1 ] = useState(true);
+  const [installPrompt , setInstallPrompt] = useState(null);
+
+  useEffect( () => {
+    console.log("Listening for Install prompt");
+    window.addEventListener('beforeinstallprompt',e=>{
+      // For older browsers
+      e.preventDefault();
+      console.log("Install Prompt fired");
+      setInstallPrompt(e);
+      console.log(e);
+      // See if the app is already installed, in that case, do nothing
+      if((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || window.navigator.standalone === true){
+        return false;
+      }
+      
+      handleToggle(true);
+                  
+      // Set the state variable to make button visible
+    })
+    
+  },[]);
+
+  const installApp = async() => {
+    if(!installPrompt) return false;
+    handleToggle(false);
+    handleToggle1(false);
+    installPrompt.prompt();
+    let outcome = await installPrompt.userChoice;    
+    if(outcome.outcome === 'accepted'){
+      console.log("App Installed")
+    }
+    else{
+      console.log("App not installed");
+    }
+    // Remove the event reference
+    setInstallPrompt(null);
+    // Hide the button
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {
+          toggle && toggle1 ?
+          <button onClick={installApp} className="add-button">Saran</button> 
+          :
+          null
+        }        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
